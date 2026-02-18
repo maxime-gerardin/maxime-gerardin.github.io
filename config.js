fec34bf516560e04bc73bf90068b06bee595b553
Maxime Gerardin <maximegerardincontact@gmail.com>
2026-02-18 23:43:38 +0100

    Add projects

--- config.js
+++ config.js
@@ -1,11 +1,20 @@
 portfolioTemplate = {
     info: {
         fullName: "Maxime Gerardin",
-        personalDescription: "3D and Environment freelance artist based in south of France, creating immersive visuals for personal projects and brands.",
+        personalDescription: "3D Environment Generalist artist based in the south of France, creating immersive visuals for Music Clips, Ads, Films and Series",
         logo: "./static/assets/logo.jpg",
         logoCircle: true,
         logoAlt: "Logo icon",
-        backgroundVideo: "./static/assets/thumbnails/secret-lab.mp4",
+        backgroundVideo: "./static/assets/thumbnails/mainTitle.mp4",
+    },
+    softwares: {
+        "Blender": "Blender.png",
+        "DaVinci Resolve": "DaVinci-Resolve.png",
+        "Gaea": "Gaea.png",
+        "Houdini" : "Houdini.png",
+        "Photoshop": "Photoshop.png",
+        "RealityScan": "RealityScan.png",
+        "SpeedTree": "SpeedTree.png",
     },
     socials: [
         {
@@ -40,10 +49,11 @@ portfolioTemplate = {
             main: true,
             videoThumbnail: "./static/assets/thumbnails/valorant.mp4",
             imgMiniThumbnail: "https://cdnb.artstation.com/p/assets/covers/images/089/449/851/smaller_square/maxime-gerardin-maxime-gerardin-thumbnail-1-1-1.jpg?1750975661",
-            tags: ["3D", "Environments"],
+            tags: ["Environments"],
             year: "2024",
             description: "Valorant worlds championship video clip SUPERPOWER",
             client: "Riot Games",
+            software: ["Blender"],
             links: [
                 {
                     url: "https://www.youtube.com/watch?v=DX4BE9GmpH4&list=RDDX4BE9GmpH4&start_radio=1",
@@ -62,13 +72,34 @@ portfolioTemplate = {
             ],
         },
         {
+            name: "Machine Hall",
+            videoThumbnail: "./static/assets/thumbnails/big-machine.mp4",
+            imgMiniThumbnail: "https://cdnb.artstation.com/p/assets/covers/images/092/135/675/smaller_square/maxime-gerardin-maxime-gerardin-still-2025-09-25-111115-3-1-1.jpg?1758793697",
+            tags: ["All Aspects"],
+            year: "2025",
+            description:    "This short project originated from just playing around, cutting shapes from textures.com photos and turning them into sloppy 3d models. "+
+                            "I ended up making the cylindrical thing and then planned to throw it away. "+
+                            "After seeing some enthusiasm for this project from friends and fellow artists, I decided to give it 2 more days of work. "+
+                            "So this project is between 2 and 3 days of work total. A lot of people ask me how I do my projects so fast, but the truth"+
+                            "is I spend 90% of my time experimenting on small scenes, different aspects of 3d, not affraid of failing because they're not real projects, "+
+                            "and that's usually how you can get easily creative ! Sometimes, I accidentally make something cool and just push it a bit further, but there is "+
+                            "a sentence that says \"You do 80% of the work in 20% of the time, and 20% of the rest of the work in 80% of the time\". For me, this means this "+
+                            "project received the first 80% of love, but I'm too lazy to finetune the remaining 20% on this side experiment project.",
+            client: "",
+            software: ["Blender"],
+            links: [],
+            medias: [],
+            main: true,
+        },
+        {
             name: "Island",
             videoThumbnail: "./static/assets/thumbnails/island.mp4",
             imgMiniThumbnail: "https://cdna.artstation.com/p/assets/video_clips/images/090/714/606/20250808092912/smaller_square/maxime-gerardin-thumb.jpg?1754663353",
-            tags: ["3D", "Environments"],
-            year: "2024",
+            tags: ["All Aspects"],
+            year: "2025",
             description: "Island environment",
             client: "",
+            software: ["Blender"],
             links: [],
             medias: [],
         },
@@ -83,6 +114,7 @@ portfolioTemplate = {
             description: "Hello, here's an 3D render made a year ago. " +
                          "I originally just wanted to help my girlfriend on her 3D Assignment for her art school",
             client: "",
+            software: ["Blender"],
             links: [
                 {
                     url: "https://www.artstation.com/artwork/mAn3De",
@@ -119,10 +151,11 @@ portfolioTemplate = {
             name: "Frozen Bridge",
             videoThumbnail: "./static/assets/thumbnails/horse.mp4",
             imgMiniThumbnail: "https://cdnb.artstation.com/p/assets/video_clips/images/084/424/851/20250131064131/smaller_square/maxime-gerardin-thumb.jpg?1738327292",
-            tags: ["3D", "Animation"],
+            tags: ["3D"],
             year: "2025",
             description: "Here's a different shot I made to get a full grasp at my previous artwork 'Winter Bridge' inspired by Bram Lepelaar concept art",
             client: "",
+            software: ["Blender","Speedtree"],
             links: [
                 {
                     url: "https://www.artstation.com/artwork/VdzaQZ",
@@ -134,6 +167,7 @@ portfolioTemplate = {
                 },
             ],
             medias: [],
+            main: true,
         }
     ],
     footer: {
@@ -154,5 +188,6 @@ portfolioTemplate = {
 
 styleConfig = {
     portfolioFont: { name: 'Outfit', weights: '100..900', useWeight: '400' }, // Google fonts
-    fullNameFont: { name: 'Outfit', weights: '100..900', useWeight: '900'} // Google fonts
+    fullNameFont: { name: 'Outfit', weights: '100..900', useWeight: '900'}, // Google fonts
+    projectDescriptionMaxLine: 3,
 }
