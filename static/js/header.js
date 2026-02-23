ee766595a372fbc32fd7a539efdce259410f190e
Dorian Gerardin <dorian.gerardin@gmail.com>
2026-02-23 14:28:21 +0100

    Add of burger menu

--- static/js/header.js
+++ static/js/header.js
@@ -1,13 +1,21 @@
 class PortfolioHeader extends HTMLElement {
 
-  createMenu(container) {
+  createMenu(container, fullmenuContainer) {
     PortfolioMenu().forEach((menuItem) => {
       if (menuItem?.home !== true) {
         let menuLink = document.createElement("a");
-        menuLink.classList.add("project-menu", "underline");
+        menuLink.classList.add("menu-link", "underline");
         menuLink.href = menuItem.url;
         menuLink.innerHTML = menuItem.text;
         container.appendChild(menuLink);
+        
+        let burgerMenuLink = document.createElement("a");
+        burgerMenuLink.href = menuItem.url
+        burgerMenuLink.innerHTML = menuItem.text
+        let burgerMenuLinkWrapper = document.createElement("div");
+        burgerMenuLinkWrapper.classList.add("burger-menu-link-wrapper")
+        burgerMenuLinkWrapper.appendChild(burgerMenuLink)
+        fullmenuContainer.appendChild(burgerMenuLinkWrapper)
       }
     })
   }
@@ -19,9 +27,39 @@ class PortfolioHeader extends HTMLElement {
             <img id="portfolio-logo" src="" alt="">
         </a>
         <div id="header-menu"></div>
+        <div id="header-burger-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div id="header-fullscreen-menu">
+          <div id="burger-menu-close-btn"><img src="static/assets/icons/cross.svg"></div>
+          <div id="burger-menu-links"></div>
+        </div>
       </div>`;
 
-      this.createMenu(document.getElementById("header-menu"))
+      this.createMenu(document.getElementById("header-menu"), document.getElementById("burger-menu-links"))
+
+      const burger = document.getElementById("header-burger-menu");
+      const menu = document.getElementById("header-fullscreen-menu");
+      const closeBtn = document.getElementById("burger-menu-close-btn");
+      const links = document.querySelectorAll("#burger-menu-links a");
+
+      burger.addEventListener("click", () => {
+        document.body.style.overflow = "hidden"
+        menu.classList.add("active");
+        links.forEach((link, index) => {
+          setTimeout(() => {
+            link.classList.add("show");
+          }, index * 175);
+        });
+      });
+      
+      closeBtn.addEventListener("click", () => {
+        document.body.style.overflow = "inherit"
+        links.forEach(link => link.classList.remove("show"));
+        menu.classList.remove("active");
+      });
 
       let portfolioLogo = document.getElementById("portfolio-logo");
       if("info" in portfolioTemplate) {
